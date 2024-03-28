e30d96df39813c5423380db620adc2898eef7325
Bethany Ingraham <BethFIngraham@gmail.com>
2024-03-28 19:17:38 -0400

    getting data from server and starting form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  
+  state = {
+      todos: []
+  }
+
+  componentDidMount() {
+    axios
+    .get(URL)
+    .then((res) => this.setState({...this.state, todos: res.data.data}))
+    .catch((err) => console.error(err))
+  }
+
+  toggleCompleted() {
+
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
-    return null
+    return (
+      <div className='App'>
+      <h1>Todo App:</h1>
+      {this.state.todos.map((todo) => {
+        return <div key={todo.id}>{todo.name}</div>
+        })}
+     <form onSubmit={this.handleSubmit}>
+      <input type='text' placeholder='type task here'/>
+      <button type='submit'>Add Todo</button>
+      <button>Clear Completed</button>
+     </form>
+      </div>
+    )
   }
 }
